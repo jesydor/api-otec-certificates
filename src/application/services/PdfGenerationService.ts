fb165d8e2edafc16318fa33c84913c700a0176f4
Jeremy Cid <jcid.rojas@gmail.com>
2024-01-17 13:08:30 -0300

    fix generatePdf function

--- src/application/services/PdfGenerationService.ts
+++ src/application/services/PdfGenerationService.ts
@@ -7,13 +7,14 @@ class PdfGenerationService {
     const template = handlebars.compile(htmlTemplate);
     const html = template(data);
 
-    const browser = await puppeteer.launch({ headless: "new", args: ['--enable-local-file-accesses'] });
+    const browser = await puppeteer.launch({ headless: "new", args: ['--enable-local-file-accesses', '--no-sandbox'] });
     const page = await browser.newPage();
 
     await page.setContent(html);
+    const pdf = await page.pdf({ format: 'Legal' });
     await browser.close();
 
-    return page.pdf({ format: 'Legal' });
+    return pdf;
   }
 }
 
