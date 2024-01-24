7197955ab335a7ad43927276f4007c4630d6399b
Jeremy Cid <jcid.rojas@gmail.com>
2024-01-24 13:30:38 -0300

    removed unused files

--- src/application/ports/IDocumentRepository.ts
+++
@@ -1,5 +0,0 @@
-import { DocumentInfo } from "../../domain/entities/DocumentInfo";
-
-export interface IDocumentRepository {
-  save(documentInfo: DocumentInfo): Promise<string>; 
-}
--- src/domain/ports/IDataStorageRepository.ts
+++
@@ -1,5 +0,0 @@
-interface IDataStorageRepository {
-    get(certificateId: string, withReport: boolean): Promise<any>;
-}
-
-export default IDataStorageRepository;
