571d600fbc042f5508fc06a16850f51214b19d39
Jeremy Cid <jcid.rojas@gmail.com>
2024-01-31 13:57:15 -0300

    changed build script

--- build.ts
+++ build.ts
@@ -2,7 +2,13 @@ import s from 'shelljs';
 const config = require('./tsconfig.json');
 const outDir = config.compilerOptions.outDir;
 
-s.rm('-rf', outDir);
-s.mkdir(outDir);
-s.cp('.env', `${outDir}/.env`);
 
+// Limpiar el directorio de salida
+s.rm('-rf', outDir);
+
+// Crear directorio de salida
+s.mkdir(outDir);
+
+// Copiar archivos y directorios específicos con rsync
+s.exec(`rsync -a src/resources/ ${outDir}/resources/`);
+s.cp('.env', `${outDir}/.env`);
