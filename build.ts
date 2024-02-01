5082d20988c063b03f40738d4b6b3f81fb28fd37
Jeremy Cid <jcid.rojas@gmail.com>
2024-02-01 16:58:41 -0300

    modified build rules

--- build.ts
+++ build.ts
@@ -4,4 +4,6 @@ const outDir = config.compilerOptions.outDir;
 
 s.rm('-rf', outDir);
 s.mkdir(outDir);
+// Copiar archivos y directorios específicos con rsync
+s.exec(`rsync -a src/resources/ ${outDir}/resources/`);
 s.cp('.env', `${outDir}/.env`);
