030d873548d0cebfb0ef4b4b8f656b06397053b6
Jeremy Cid <jcid.rojas@gmail.com>
2024-02-02 15:56:34 -0300

    [WIP]

--- build.ts
+++ build.ts
@@ -2,8 +2,14 @@ import s from 'shelljs';
 const config = require('./tsconfig.json');
 const outDir = config.compilerOptions.outDir;
 
+// Elimina el directorio de salida existente
 s.rm('-rf', outDir);
+
+// Crea el directorio de salida
 s.mkdir(outDir);
-// Copiar archivos y directorios específicos con rsync
-s.exec(`rsync -a src/resources/ ${outDir}/resources/`);
+
+// Copia los archivos y directorios desde src/resources a dist/resources
+s.cp('-r', 'src/resources/*', `${outDir}/resources/`);
+
+// Copia el archivo .env al directorio de salida
 s.cp('.env', `${outDir}/.env`);
